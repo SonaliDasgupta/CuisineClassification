6c11a24bdc2adcc6a0447f93a7ec8e6a1e3173cc
SonaliDasgupta <sonalidasgupta95.2011@gmail.com>
2019-02-02 19:50:13 +0530

    Flask Web ML App

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, url_for, request, render_template
+import requests
+from io import StringIO
+from PIL import Image
+from path import Path
+import numpy as np
+import os
+import torch
+from torch import nn
+from torchvision import datasets, transforms, models
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods = ['POST'])
+def predict():
+    if request.method == 'POST':
+        url = request.form['location']
+        response = requests.get(url)
+        test_transforms = transforms.Compose([transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+        [0.229, 0.224, 0.225])
+        ])
+
+        classes = ['Indian', 'Italian', 'Mexican']
+        model = torch.load('cuisinemodel_(2).pth')
+        image = Image.open(StringIO(response.content))
+        input = test_transforms(image)
+        input = input.view(1, 3, 224, 224)
+        output = model(input)
+        prediction = torch.argmax(output.data)
+    return render_template('results.html', prediction = classes[prediction])
+
+if __name__=='__main__':
+    app.run(host = '127.0.0.1', port = 5000, debug = True)
+
+   
